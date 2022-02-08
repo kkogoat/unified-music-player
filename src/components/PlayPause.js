11b7fd414c96d0d155793533660c7441b836d6e2
AlteredReality <38482035+AlteredReality@users.noreply.github.com>
2022-02-07 20:49:15 -0800

    Added feature to default to accepting audio files from file explorer
    When the file explorer opens, the file extensions it will only accept by default are audio files

--- src/components/PlayPause.js
+++ src/components/PlayPause.js
@@ -3,31 +3,32 @@ import '../css/PlayPause.css'
 export class PlayPause extends Component {
     constructor(props) {
         super(props);
-        this.state = { player: '' }
-        this.handleChange = this.handleChange.bind(this)
-        this.selectLocal = this.selectLocal.bind(this)
-        this.play = this.play.bind(this)
-        this.pause = this.pause.bind(this)
+        this.state = { player: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.selectLocal = this.selectLocal.bind(this);
+        this.play = this.play.bind(this);
+        this.pause = this.pause.bind(this);
     }
     handleChange(e) {
-        const filePath = URL.createObjectURL(e.target.files[0])
-        const playFile = new Audio(filePath)
-        this.setState({ player: playFile })
+        const filePath = URL.createObjectURL(e.target.files[0]);
+        const playFile = new Audio(filePath);
+        this.setState({ player: playFile });
     }
     // function to play audio saved on computer
     play() {
-        this.state.player.play()
+        this.state.player.play();
     }
     // function to pause
     pause() {
-        this.state.player.pause()
+        this.state.player.pause();
     }
-    // this function opens the file explorer but when the mp3 file is selected it is not playing it
+    //function to open file explorer and select audio file
     selectLocal() {
         var input = document.createElement('input');
         input.type = 'file';
+        input.accept ="audio/*"; //when the file explorer opens, only allow the user to select files with this extension
         input.onchange = e => {
-            this.handleChange(e)
+            this.handleChange(e);
         }
         input.click();
     }
